9e41f34becc66a25264978a3d3c76daeff4afb31
yifan.li <liyifan14@outlook.com>
2026-06-13 22:42:50 +0100

    reset

--- expense-data.js
+++ expense-data.js
@@ -10,4 +10,8 @@ window.expenseCsvData = `date,person,amount,currency,category,note
 20260612,Yixin,0.87,GBP,Food,bananas at tesco
 20260612,Yixin,5.4,GBP,Food,macha latte at gails
 20260612,Yixin,5.9,GBP,Food,hey tea
-20260612,Yifan,7.99,GBP,Food,chicken wings at popeyes`;
+20260612,Yifan,7.99,GBP,Food,chicken wings at popeyes
+20260613,Yifan,7.2,GBP,Transport,transport fot london in 20260612
+20260613,Yifan,10.41,GBP,Food,cherries and sausages at M&S
+20260613,Yixin,7.2,GBP,Transport,transport fot london in 20260612
+20260613,Yixin,3.77,GBP,Food,cherry tomatoes and cucumber at Tesco`;
