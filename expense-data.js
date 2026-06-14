b101cd0453d072ebf4c766472dbeae04ffa3db61
yifan.li <liyifan14@outlook.com>
2026-06-14 22:27:54 +0100

    update 20260614

--- expense-data.js
+++ expense-data.js
@@ -1,8 +1,14 @@
 window.expenseCsvData = `date,person,amount,currency,category,note
-20260611,Yixin,9.6,GBP,Food,yogurt and tomatoes at tesco
-20260611,Yixin,6.6,GBP,Food,bread at gails
-20260611,Yixin,4.25,GBP,Food,at M&S
-20260611,Yixin,45,GBP,Food,at jinshi
+20260614,Yifan,5.6,GBP,Food,iced americano starbuscks
+20260614,Yifan,34.4,GBP,Food,pan-fried dumplings
+20260614,Yifan,4.9,GBP,Food,oat milk at tesco
+20260614,Yixin,9.7,GBP,Food,chinese supermarket
+20260614,Yixin,4.1,GBP,Food,gails
+20260614,Yixin,1.65,GBP,Food,onions and garlic at tesco
+20260613,Yifan,7.2,GBP,Transport,transport fot london in 20260612
+20260613,Yifan,10.41,GBP,Food,cherries and sausages at M&S
+20260613,Yixin,7.2,GBP,Transport,transport fot london in 20260612
+20260613,Yixin,3.77,GBP,Food,cherry tomatoes and cucumber at Tesco
 20260612,Yifan,2.5,GBP,Transport,dlr from lcy to woolwich on 20260611
 20260612,Yifan,64,GBP,Food,old town 97
 20260612,Yifan,37.31,GBP,Food,vegetables and meat and fruit
@@ -11,7 +17,7 @@ window.expenseCsvData = `date,person,amount,currency,category,note
 20260612,Yixin,5.4,GBP,Food,macha latte at gails
 20260612,Yixin,5.9,GBP,Food,hey tea
 20260612,Yifan,7.99,GBP,Food,chicken wings at popeyes
-20260613,Yifan,7.2,GBP,Transport,transport fot london in 20260612
-20260613,Yifan,10.41,GBP,Food,cherries and sausages at M&S
-20260613,Yixin,7.2,GBP,Transport,transport fot london in 20260612
-20260613,Yixin,3.77,GBP,Food,cherry tomatoes and cucumber at Tesco`;
+20260611,Yixin,9.6,GBP,Food,yogurt and tomatoes at tesco
+20260611,Yixin,6.6,GBP,Food,bread at gails
+20260611,Yixin,4.25,GBP,Food,at M&S
+20260611,Yixin,45,GBP,Food,at jinshi`;
