37aa817d66862e51a431910a6c5665aee3c8a2d2
yifan.li <liyifan14@outlook.com>
2026-06-19 20:10:22 +0100

    updated

--- expense-data.js
+++ expense-data.js
@@ -1,4 +1,22 @@
 window.expenseCsvData = `date,person,amount,currency,category,note
+20260619,Yixin,10,GBP,Service,dry cleaning
+20260619,Yifan,12.8,GBP,Transport,tfl on 20260618
+20260619,Yifan,59.49,GBP,Food,yiban at london
+20260619,Yifan,4.5,GBP,Food,oat milk macha latte
+20260619,Yifan,7.48,GBP,Food,juice and baby tomato and cucumber
+20260619,Yifan,12.8,GBP,Transport,tfl on 20260618
+20260619,Yixin,12.8,GBP,Transport,tfl on 20260618
+20260619,Yixin,5.7,GBP,Food,starbucks
+20260619,Yixin,100.05,GBP,Shopping,clothes
+20260618,Yifan,2.2,GBP,Food,oat milk
+20260618,Yixin,18,GBP,Shopping,flowers for colleagues
+20260618,Yixin,4.6,GBP,Transport,tfl on 20260617
+20260618,Yixin,4,GBP,Food,gails
+20260618,Yixin,5.65,GBP,Food,starbucks
+20260618,Yifan,4.6,GBP,Transport,tfl on 20260617
+20260618,Yifan,54.45,GBP,Food,rasa sayang restaurant
+20260618,Yifan,15.6,GBP,Food,cakes
+20260618,Yifan,1.6,GBP,Food,ices
 20260617,Yifan,64.13,GBP,Food,snacks
 20260617,Yixin,10,GBP,Service,social activity boni
 20260617,Yixin,4,GBP,Food,gails
@@ -7,8 +25,8 @@ window.expenseCsvData = `date,person,amount,currency,category,note
 20260615,Yixin,9.08,GBP,Food,lidl
 20260615,Yixin,1.39,GBP,Food,tesco
 20260615,Yixin,6.75,GBP,Food,vagetables
-20260615,Yixin,7.2,GBP,Transport,tfl in 20260614
-20260615,Yifan,7.2,GBP,Transport,tfl in 20260614
+20260615,Yixin,7.2,GBP,Transport,tfl on 20260614
+20260615,Yifan,7.2,GBP,Transport,tfl on 20260614
 20260615,Yifan,20.0,GBP,Food,sea bass and shrimp
 20260614,Yifan,5.6,GBP,Food,iced americano starbucks
 20260614,Yifan,34.4,GBP,Food,pan-fried dumplings
@@ -16,9 +34,9 @@ window.expenseCsvData = `date,person,amount,currency,category,note
 20260614,Yixin,9.7,GBP,Food,chinese supermarket
 20260614,Yixin,4.1,GBP,Food,gails
 20260614,Yixin,1.65,GBP,Food,onions and garlic at tesco
-20260613,Yifan,7.2,GBP,Transport,transport for london in 20260612
+20260613,Yifan,7.2,GBP,Transport,transport for london on 20260612
 20260613,Yifan,10.41,GBP,Food,cherries and sausages at M&S
-20260613,Yixin,7.2,GBP,Transport,transport for london in 20260612
+20260613,Yixin,7.2,GBP,Transport,transport for london on 20260612
 20260613,Yixin,3.77,GBP,Food,cherry tomatoes and cucumber at Tesco
 20260612,Yifan,2.5,GBP,Transport,dlr from lcy to woolwich on 20260611
 20260612,Yifan,64,GBP,Food,old town 97
